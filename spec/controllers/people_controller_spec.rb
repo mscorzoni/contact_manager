2aca541c1eeb3c698af5de6465f41e93aaf3622a
Marcos Scorzoni <mlessa@gmail.com>
2018-08-07 22:37:46 +0100

    add tests for validates presence for first_name and last_name

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe PeopleController, type: :controller do
   let(:valid_attributes) {
     { first_name: 'Jane', last_name: 'Doe' }
   }
+
+  let(:invalid_attributes) {
+    { first_name: nil, last_name: nil }
+  }
 end
