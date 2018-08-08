7d175d0477d871a8b929526c44478ad524c9710d
Marcos Scorzoni <mlessa@gmail.com>
2018-08-08 10:55:32 +0100

    add validations for phone_number

--- spec/controllers/phone_numbers_controller_spec.rb
+++ spec/controllers/phone_numbers_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe PhoneNumbersController, type: :controller do
+  let(:valid_attributes) do
+    {
+      number: "MyString",
+      person_id: 1
+    }
+ end
 
+  let(:invalid_attributes) do
+    {
+      number: nil,
+      person_id: nil
+    }
+  end
 end
--- spec/models/phone_number_spec.rb
+++ spec/models/phone_number_spec.rb
@@ -1,9 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe PhoneNumber, type: :model do
-  let(:phone_number) do
-    PhoneNumber.new(number: "555555555")
+  let(:person) do
+      Person.create!(
+                  first_name: 'First',
+                  last_name: 'Last'
+                 )
   end
+
+
+  let(:phone_number) do
+    PhoneNumber.create!(number: "555555555",
+                    person_id: person.id
+                    )
+  end
+
 
   it 'is valid' do
     expect(phone_number).to be_valid
@@ -12,5 +23,10 @@ RSpec.describe PhoneNumber, type: :model do
   it 'is invalid without a number' do
     phone_number.number = nil
     expect(phone_number).to_not be_valid
+  end
+
+  it 'must have a reference to a person' do
+    phone_number.person_id = nil
+    expect(phone_number).not_to be_valid
   end
 end
