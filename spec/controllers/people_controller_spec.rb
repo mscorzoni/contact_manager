4702748a6ebf1d33a0a99c89d94398d44f52f4c2
Marcos Scorzoni <mlessa@gmail.com>
2018-08-08 08:13:09 +0100

    fix false positive in person specs

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -8,4 +8,17 @@ RSpec.describe PeopleController, type: :controller do
   let(:invalid_attributes) {
     { first_name: nil, last_name: nil }
   }
+
+  # let(:new_attributes) {
+  #   { first_name: 'NewFirstName', last_name: 'NewLastName' }
+  # }
+
+
+  # it "updates the requested person" do
+  # person = Person.create! valid_attributes
+  # put :update, {:id => person.to_param, :person => new_attributes}, valid_session
+  # person.reload
+  # expect(person.first_name).to eq('NewFirstName')
+  # expect(person.last_name).to eq('NewLastName')
+  # end
 end
